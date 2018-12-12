fce56c7369c1b4a3519b9a46bc4b379463b45682
Dmitry Barskov <barskoidim@gmail.com>
2018-12-12 20:40:25 +0300

    paint color bg black

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,17 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    final static int S_WIDTH = 800;
-    final static int S_HEIGHT = 600;
+    private final static int S_WIDTH = 800;
+    private final static int S_HEIGHT = 600;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Pong!");
-        primaryStage.setScene(new Scene(root, S_WIDTH, S_HEIGHT));
+        Scene scene = new Scene(root, S_WIDTH, S_HEIGHT, Color.BLACK);
+        primaryStage.setScene(scene);
         primaryStage.setResizable(false);
 
         primaryStage.show();
