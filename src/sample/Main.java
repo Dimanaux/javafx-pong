a3e899d6371ff7b3fd2c291d0e5dc73e3d363e68
cosmoskey <barskoidim@gmail.com>
2018-12-15 20:52:16 +0300

    Add paddles to the screen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,8 +2,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -13,11 +13,19 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        AnchorPane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Pong!");
         Scene scene = new Scene(root, S_WIDTH, S_HEIGHT, Color.BLACK);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
+
+        final int S_CENTER = S_HEIGHT / 2 - Paddle.DEFAULT_HEIGHT / 2;
+
+        Paddle p = new Paddle(20, S_CENTER);
+        root.getChildren().add(p.asView());
+
+        Paddle p2 = new Paddle(S_WIDTH - 20 - Paddle.DEFAULT_WIDTH, S_CENTER);
+        root.getChildren().add(p2.asView());
 
         primaryStage.show();
     }
