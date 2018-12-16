9e819547827a3a123eb3e268e8eae657792e6cd5
cosmoskey <barskoidim@gmail.com>
2018-12-16 22:50:45 +0300

    create client class

---
+++ src/sample/Slave.java
@@ -0,0 +1,17 @@
+package sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Slave extends Computer implements Observable, Observer {
+    Slave(String hostName) {
+        try {
+            super.socket = new Socket(hostName, DEFAULT_PORT);
+            super.writer = new PrintWriter(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Cannot connect to " + hostName);
+        }
+    }
+}
