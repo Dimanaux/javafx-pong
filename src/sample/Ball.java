6596290d69c12a19a0979dc8742100cfdbbc8e13
cosmoskey <barskoidim@gmail.com>
2018-12-16 12:40:42 +0300

    create class Ball (view)

---
+++ src/sample/Ball.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Ball {
+    public static final int LENGTH = 20;
+
+    private Rectangle ball;
+
+    public Ball() {
+        ball = new Rectangle(
+                LENGTH,
+                LENGTH,
+                Color.WHITE
+        );
+    }
+
+    public Ball(double x, double y) {
+        this();
+        ball.setX(x);
+        ball.setY(y);
+    }
+
+    public Node asNode() {
+        return ball;
+    }
+}
