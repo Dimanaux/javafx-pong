f12c9629c5612ae7440766edb12f865ba8dcc225
cosmoskey <barskoidim@gmail.com>
2018-12-24 14:31:33 +0300

    direction vertical class

--- src/main/java/com/example/pong/DirectionVertical.java
+++ src/main/java/com/example/pong/DirectionVertical.java
@@ -1,4 +1,16 @@
 package com.example.pong;
 
 public enum DirectionVertical {
+    UP, DOWN;
+
+    private DirectionVertical opposite;
+
+    static {
+        UP.opposite = DOWN;
+        DOWN.opposite = UP;
+    }
+
+    public DirectionVertical opposite() {
+        return opposite;
+    }
 }
